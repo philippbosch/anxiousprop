075cb085a4129753a459a8dab285322c714699b2
Philipp Bosch <pb@philippbosch.de>
2010-10-01 13:14:46 +0200

    JSONified app.

--- anxiousprop.py
+++ anxiousprop.py
@@ -6,12 +6,13 @@ import json
 import os
 import random
 
-from flask import Flask, render_template, url_for, abort, g, redirect, make_response, send_from_directory
+from flask import Flask, render_template, url_for, abort, g, redirect, make_response, send_from_directory, jsonify
 from werkzeug.exceptions import NotFound
 
 app = Flask(__name__)
 
 DEBUG = os.environ.get('FLASK_DEBUG', False)
+USE_X_SENDFILE = True
 SECRET_KEY = "\xef\x1e,X\xb3\xae#\x7f\xa5\xa6\xec]7\xc6@\x03\x8cj\x99{\x95\xec\x85g"
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 PDF_DIRECTORY = os.path.join(PROJECT_ROOT, app.static_path[1:], 'pdf')
@@ -22,25 +23,26 @@ app.config.from_object(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return jsonify(status="404 Not found")
 
 @app.route("/randomize-publication/")
 def randomize_publication():
     queue_id = md5(os.urandom(24)).hexdigest()
-    beanstalk = beanstalkc.Connection()
     pages = ["%02d" % page_num for page_num in range(2, app.config['PDF_NUM_PAGES']+1)]
     random.shuffle(pages)
-    beanstalk.put(json.dumps({'queue_id': queue_id, 'pages': pages}))
-    return redirect(url_for('pick_up_publication', queue_id=queue_id))
-
+    try:
+        beanstalk = beanstalkc.Connection()
+        beanstalk.put(json.dumps({'queue_id': queue_id, 'pages': pages}))
+    except Exception, e:
+        return jsonify(status="fail", message="%s" % e)
+    return jsonify(status="ok", queue_id=queue_id, pick_up_url=url_for('pick_up_publication', queue_id=queue_id))
+    
 @app.route("/publications/<queue_id>")
 def pick_up_publication(queue_id):
     if os.path.exists(os.path.join(app.config['PDF_DIRECTORY'], '%s.pdf' % queue_id)):
-        return render_template("pick-up-ready.html", queue_id=queue_id)
+        return jsonify(status="ready", pdf_url=url_for('download_publication', queue_id=queue_id))
     else:
-        response = make_response(render_template("pick-up-waiting.html", queue_id=queue_id))
-        response.headers['Refresh'] = 3
-        return response
+        return jsonify(status="processing")
 
 @app.route("/publications/<queue_id>/download")
 def download_publication(queue_id):
