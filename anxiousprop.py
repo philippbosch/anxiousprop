7381f0970154ae1a52c588538733c938aabd25be
Philipp Bosch <hello@pb.io>
2014-04-18 00:11:14 +0200

    Strip down.

--- anxiousprop.py
+++ anxiousprop.py
@@ -1,24 +1,26 @@
 # -*- coding: utf-8 -*-
 
-import beanstalkc
 from hashlib import md5
+from io import BytesIO
 import json
 import os
 import random
 
-from flask import Flask, render_template, url_for, abort, g, redirect, make_response, send_from_directory, jsonify, request
+from flask import Flask, render_template, url_for, abort, g, redirect, make_response, send_from_directory, jsonify, request, Response
+from pyPdf import PdfFileWriter, PdfFileReader
 from werkzeug.exceptions import NotFound
+
 
 app = Flask(__name__)
 
 DEBUG = os.environ.get('FLASK_DEBUG', False)
-#USE_X_SENDFILE = True
 SECRET_KEY = "\xef\x1e,X\xb3\xae#\x7f\xa5\xa6\xec]7\xc6@\x03\x8cj\x99{\x95\xec\x85g"
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-PDF_DIRECTORY = os.path.join(PROJECT_ROOT, app.static_path[1:], 'pdf')
+PDF_SRC_DIR = os.path.join(PROJECT_ROOT, 'static', 'pdf-src')
 PDF_NUM_PAGES = 17
 
 app.config.from_object(__name__)
+
 
 def jsonpify(*args, **kwargs):
     resp = jsonify(*args, **kwargs)
@@ -26,36 +28,40 @@ def jsonpify(*args, **kwargs):
         resp.data = "%s(%s);" % (request.args['callback'], resp.data)
     return resp
 
+
 @app.route("/")
 def index():
     return jsonpify(status="404 Not found")
 
+
 @app.route("/randomize-publication/")
 def randomize_publication():
     queue_id = md5(os.urandom(24)).hexdigest()
-    pages = ["%02d" % page_num for page_num in range(2, app.config['PDF_NUM_PAGES']+1)]
-    random.shuffle(pages)
-    try:
-        beanstalk = beanstalkc.Connection()
-        beanstalk.put(json.dumps({'queue_id': queue_id, 'pages': pages}))
-    except Exception, e:
-        return jsonpify(status="fail", message="%s" % e)
     return jsonpify(status="ok", queue_id=queue_id, pick_up_url=url_for('pick_up_publication', queue_id=queue_id))
-    
+
+
 @app.route("/publications/<queue_id>")
 def pick_up_publication(queue_id):
-    if os.path.exists(os.path.join(app.config['PDF_DIRECTORY'], '%s.pdf' % queue_id)):
-        print request.host_url
-        return jsonpify(status="ready", pdf_url="%s%s" % (request.host_url[:-1], url_for('download_publication', queue_id=queue_id)))
-    else:
-        return jsonpify(status="processing")
+    return jsonpify(status="ready", pdf_url="%s%s" % (request.host_url[:-1], url_for('download_publication', queue_id=queue_id)))
+
 
 @app.route("/publications/<queue_id>/download")
 def download_publication(queue_id):
-    try:
-        return send_from_directory(app.config['PDF_DIRECTORY'], '%s.pdf' % queue_id, as_attachment=True, attachment_filename="The Anxious Prop - Case 3.pdf")
-    except NotFound:
-        return redirect(url_for('pick_up_publication', queue_id=queue_id))
+    pages = ["%02d" % page_num for page_num in range(2, app.config['PDF_NUM_PAGES']+1)]
+    random.shuffle(pages)
+    page_files = ["%s/%s.pdf" % (PDF_SRC_DIR, page) for page in ['01'] + pages]
+
+    output = PdfFileWriter()
+    for page_file in page_files:
+        page_pdf = PdfFileReader(file(page_file, "rb"))
+        output.addPage(page_pdf.getPage(0))
+
+    outputStream = BytesIO()
+    output.write(outputStream)
+    outputStream.seek(0)
+
+    return Response(outputStream, mimetype='application/pdf')
+
 
 if __name__ == "__main__":
-    app.run('0.0.0.0')
+    app.run('0.0.0.0', debug=True)
